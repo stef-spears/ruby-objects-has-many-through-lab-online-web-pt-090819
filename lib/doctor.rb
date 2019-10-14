4c7bb514a176f31b781b87b4c61ffdf0829bc5db
Stef Spears <spears.sj87@gmail.com>
2019-10-14 03:06:01 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,7 +19,7 @@ class Doctor
   end 
   
   def appointments 
-    Appointments.all.select { |appt| appt.doctor == self }
+    Appointment.all.select { |appt| appt.doctor == self }
   end 
   
   def patients 
