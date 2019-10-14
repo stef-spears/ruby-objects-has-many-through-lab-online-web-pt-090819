363f28e45edb0716167ab524d181754cbd348c3a
Stef Spears <spears.sj87@gmail.com>
2019-10-14 02:59:38 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,6 +1,19 @@
 class Patient 
-  
+  attr_accessor :name 
   @@all = []
   
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def new_appointment(doctor, date)
+    new_appt = Appointment.new()
+  end
+  
+  def self.all 
+    @@all 
+  end
   
 end 
