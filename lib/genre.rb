4dd1e3001f44a43f76f241f5ae5a21bb03912fc7
Stef Spears <spears.sj87@gmail.com>
2019-10-14 02:40:01 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Genre 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs
+    Songs.all.select {|song| song.genre == self }
+  end 
+  
+  
+end 
